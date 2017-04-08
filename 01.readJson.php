3d243463109db0d84d609f40c042f3cadbeebfbe
m7grey <m7grey@gmail.com>
2017-04-08 13:12:49 -0400

    json file into root folder win => C:/data mac cd/ // mkdir data

--- 01.readJson.php
+++ 01.readJson.php
@@ -6,7 +6,7 @@
  * Time: 12:48 PM
  */
 
-$file = fopen("/data/100.purchase.json","r");
+$file = fopen("/data/100.purchase.json","r"); //put json file in root folder no metter what
     while(! feof($file))
     {
         echo fgets($file)."<br />";
